047bff2e7c6e0f2157ea0d37097a42ff6db6089e
Sayantika Roy <120772627+sayo-134@users.noreply.github.com>
2025-06-25 17:42:29 +0530

    Create 6_priority_encoder.v

---
+++ Verilog Language/Procedures/6_priority_encoder.v
@@ -0,0 +1,14 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos  );
+    always @(*) begin
+        casex(in)
+            4'b0000: pos = 2'd0;
+            4'bxxx1: pos = 2'd0;
+            4'bxx10: pos = 2'd1;
+            4'bx100: pos = 2'd2;
+            4'b1000: pos = 2'd3;
+        endcase
+    end
+endmodule

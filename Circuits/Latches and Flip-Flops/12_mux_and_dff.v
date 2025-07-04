eb15e900780cf91906029ea09455588506075a28
Sayantika Roy <120772627+sayo-134@users.noreply.github.com>
2025-07-04 17:29:36 +0530

    Create 12_mux_and_dff.v

---
+++ Circuits/Latches and Flip-Flops/12_mux_and_dff.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+    always @(posedge clk) begin
+        if (L) Q <= R;
+        else if (E) Q <= w;
+    end
+
+endmodule

a0cd3cb8539a70ba8cfc185e260ce4ab1222a443
Sayantika Roy <120772627+sayo-134@users.noreply.github.com>
2025-07-04 17:50:03 +0530

    Update 15_detect_an_edge.v

--- Circuits/Latches and Flip-Flops/15_detect_an_edge.v
+++ Circuits/Latches and Flip-Flops/15_detect_an_edge.v
@@ -3,14 +3,11 @@ module top_module (
     input [7:0] in,
     output [7:0] pedge
 );
-    integer i;
     reg [7:0] q, pedge_reg;
     assign pedge = pedge_reg;
     always @(posedge clk) begin
-        for (i=0;i<=7;i=i+1) begin
-            q <= in;
-            pedge_reg <= in & ~q;
-        end
+        q <= in;
+        pedge_reg <= in & ~q;
     end
 
 endmodule
